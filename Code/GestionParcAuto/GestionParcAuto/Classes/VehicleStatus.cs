005352aa16b632530cac341a7dce9a32f6a403cb
Zaired <eliott.deriaz@gmail.com>
2025-05-05 14:13:07 +0200

    Contenu de base du projet

---
+++ Code/GestionParcAuto/GestionParcAuto/Classes/VehicleStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace TPI_GestionParcAutomobileEntreprise.Classes
+{
+    public class VehicleStatus
+    {
+        public readonly char SOLD = 'S';
+        public readonly char NORMAL = 'N';
+    }
+}
