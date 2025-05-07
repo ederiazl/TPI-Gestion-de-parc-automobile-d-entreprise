44b365b6316f2029286fb60e0ca1668fca41d282
Eliott Deriaz <eliott.deriaz@gmail.com>
2025-05-07 16:15:03 +0200

    Ajoute la page liste des vehicules

--- Code/GestionParcAuto/GestionParcAuto/Controllers/VehiclesController.cs
+++ Code/GestionParcAuto/GestionParcAuto/Controllers/VehiclesController.cs
@@ -1,14 +1,36 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using GestionParcAuto.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using GestionParcAuto.Classes;
 
 namespace GestionParcAuto.Controllers
 {
     [Authorize]
     public class VehiclesController : Controller
     {
-        public IActionResult Index()
+        ApplicationDbContext _context;
+
+        public VehiclesController(ApplicationDbContext context)
         {
+            _context = context;
+        }
+        public IActionResult Index(Message message)
+        {
+            if (message.Text != null)
+            {
+                ViewBag.Message = Json(message);
+            }
+
             return View();
         }
+
+        #region Data
+
+        public async Task<IActionResult> GetVehicles()
+        {
+            return new JsonResult(_context.Vehicles.Select(x => new { }));
+        }
+
+        #endregion
     }
 }
