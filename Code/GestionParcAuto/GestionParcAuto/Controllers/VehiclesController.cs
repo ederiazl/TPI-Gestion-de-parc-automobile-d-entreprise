e496d6f82601a75e407b6a0746d353588393a4f5
Eliott Deriaz <eliott.deriaz@gmail.com>
2025-05-07 13:56:57 +0200

    Corrige divers petit bugs

---
+++ Code/GestionParcAuto/GestionParcAuto/Controllers/VehiclesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestionParcAuto.Controllers
+{
+    [Authorize]
+    public class VehiclesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Code/GestionParcAuto/GestionParcAuto/Program.cs
+++ Code/GestionParcAuto/GestionParcAuto/Program.cs
@@ -45,7 +45,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Vehicles}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 app.MapRazorPages()
